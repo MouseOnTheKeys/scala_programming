bbce7b7fc8750622f993cda8d12df3bebd5e0417
Nenad Bubalo <nenad.bubalo@outlook.com>
2020-11-15 21:04:25 +0100

    Ending week 1 EFPL course

--- Scala_EFPL/Functional_Programming_Principles/Week_1/recfun/src/main/scala/recfun/RecFun.scala
+++ Scala_EFPL/Functional_Programming_Principles/Week_1/recfun/src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,6 @@
 package recfun
 
+
 object RecFun extends RecFunInterface {
 
   def main(args: Array[String]): Unit = {
@@ -14,15 +15,47 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c <= 0 || c >= r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = def balance(chars: List[Char]): Boolean = {
+    def pennyCount(cnt:Int, chr: Char, lst: List[Char]): Int = {
+      var newCnt = cnt
+      if(chr == '(') newCnt = cnt  + 1
+      else if(chr == ')') newCnt = cnt  - 1
+
+      if(newCnt < 0) newCnt
+      else if(lst.isEmpty) newCnt
+      else pCounter(newCnt, lst.head, lst.tail)
+
+    }
+
+    pCounter(0, 0, chars) == 0
+
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChangeRec(currentMoney: Int, currentCoins: List[Int]): Int = {
+      if (currentMoney == money)
+        1
+      else if (currentMoney > money || currentCoins.isEmpty)
+        0
+      else
+        countChangeRec(currentMoney + currentCoins.head, currentCoins) +
+          countChangeRec(currentMoney, currentCoins.tail)
+    }
+
+    countChangeRec(0, coins)
+  }
 }
+
--- Scala_EFPL/Functional_Programming_Principles/Week_1/recfun_test/src/main/scala/recfun/RecFun.scala
+++ Scala_EFPL/Functional_Programming_Principles/Week_1/recfun_test/src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,5 @@
 package recfun
-
+import scala.annotation.tailrec
 object RecFun extends RecFunInterface {
 
   def main(args: Array[String]): Unit = {
@@ -14,15 +14,50 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c <= 0 || c >= r)
+      1
+    else
+      pascal(c-1, r-1) + pascal(c, r-1)
+
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+      @tailrec
+      def balanceRec(currentChars: List[Char], count: Int): Boolean = {
+      if (count < 0)
+        false
+      else if (currentChars.isEmpty)
+        count == 0
+      else if (currentChars.head == '(')
+        balanceRec(currentChars.tail, count + 1)
+      else if (currentChars.head == ')')
+        balanceRec(currentChars.tail, count - 1)
+      else
+        balanceRec(currentChars.tail, count)
+    }
+
+    balanceRec(chars, 0)
+  }
+
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChangeReccurent(curMoney: Int, curCoins: List[Int]): Int = {
+      if (curMoney == money)
+        1
+      else if (curMoney > money || curCoins.isEmpty)
+        0
+      else
+        countChangeReccurent(curMoney + curCoins.head, curCoins) +
+          countChangeRecccurent(curMoney, curCoins.tail)
+    }
+
+    countChangeReccurent(0, coins)
+  }
 }
